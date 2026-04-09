c875775603a84bdb61a3efe12f673fba01db5a87
rokaiya22 <akterrokeya785@gmail.com>
2026-04-09 15:42:34 +0900

    Add files via upload

---
+++ sensor.py
@@ -0,0 +1,29 @@
+import random as r
+
+class Sensor:
+    """
+    Base Sensor class.
+    """
+    def __init__(self, name: str) -> None:
+        self.name = name
+        
+    def read(self) -> float:
+        return 0.0 # to overwrite...
+    
+# Inheritance
+class TemperatureSensor(Sensor):
+    """
+    Simulated temp sensor.
+    """
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        
+        def read(self) -> float: # overwritten Sensor.read()
+            return round(r.uniform(20.0, 30.0), 2)
+        
+class LightSensor(Sensor):
+    """
+    Simulated light sensor.
+    """
+    def read(self) -> float: # overwritten
+        return round(r.uniform(0, 100), 2)
